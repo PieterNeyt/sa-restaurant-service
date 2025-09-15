869619a7637d29b1beea1b72be9cb331f81cd674
Hugo Dor <hugo.dor@student.kdg.be>
2025-09-15 19:56:52 +0200

    Initial Commit

---
+++ src/test/java/be/kdg/sa/deliveryservice/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kdg.sa.deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
